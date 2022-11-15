6b6b588b21054b91316ee28b44155dc8d625739b
zakve <zaklas.martin@gmail.com>
2022-11-15 20:29:34 +0100

    Add zombieOwner and ownerZombieCount

--- contracts/ZombieFactory.sol
+++ contracts/ZombieFactory.sol
@@ -13,9 +13,13 @@ contract ZombieFactory {
     }
 
     Zombie[] public zombies;
+    mapping(uint256 => address) public zombieToOwner;
+    mapping(address => uint256) ownerZombieCount;
 
     function _createZombie(string memory _name, uint256 _dna) private {
         uint256 id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
     }
 
