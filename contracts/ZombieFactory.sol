97cc65374b9b11cf10a9d6cb449d3aa44a0ccae7
zakve <zaklas.martin@gmail.com>
2022-11-15 20:33:53 +0100

    Fix zombie ID

--- contracts/ZombieFactory.sol
+++ contracts/ZombieFactory.sol
@@ -17,7 +17,8 @@ contract ZombieFactory {
     mapping(address => uint256) ownerZombieCount;
 
     function _createZombie(string memory _name, uint256 _dna) private {
-        uint256 id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombies.push(Zombie(_name, _dna));
+        uint256 id = zombies.length - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
