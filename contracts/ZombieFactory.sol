1d67093cbb9525fe812feaa7086822cb962c277c
zakve <zaklas.martin@gmail.com>
2022-11-17 19:20:56 +0100

    Fix compiler version

--- contracts/ZombieFactory.sol
+++ contracts/ZombieFactory.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.5.0 <0.9.0;
+pragma solidity >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
     event NewZombie(uint256 zombieId, string name, uint256 dna);
